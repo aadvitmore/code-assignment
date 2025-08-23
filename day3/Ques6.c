f97e03f575e374c833cb22bfe6b813b34291df2c
Aadvit More <aadvitmore@gmail.com>
2025-08-23 12:04:44 +0000

    solved questions

---
+++ day3/Ques6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1, num2, temp;
+
+    printf("Enter the numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    printf("After swap: %d %d\n", num1, num2);
+
+
+    return 0;
+}
+
